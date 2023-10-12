7e8e2571b5536160f7f65c29bfbb2038bc63e8d3
kylpvn <108206290+kylpvn@users.noreply.github.com>
2023-10-12 00:32:45 -0700

    updating database

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ const db = knex({
   // connect to your own database here:
   client: 'pg',
   connection: {
-    connectionString: process.env.DATABASE_URL,
-  ssl: true,
+    ssl: {
+      rejectUnauthorized: false
+    }
   }
   
 });
